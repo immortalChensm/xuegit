4af099da08ceba212beeccf2fa120d888c171f8f
oldshiji <1655664358@qq.com>
2018-09-03 22:32:03 +0800

    update readme.php

--- readme.php
+++ readme.php
@@ -6,12 +6,4 @@
  * Time: 20:39
  */
 
-echo 'Git is a distributed version control system.
-Git isGit is a distributed version control system.
-Git is free software distributed under the GPL.';
-
-Git has a mutable index called stage.
-Git tracks changes.
-Git tracks changes of files.
-
-My stupid boss still prefers SVN.
+sdfldslfdsjkfldskfldsfkls
